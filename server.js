f190503ef18a25e86e2e311560fa08fe45aed057
siinemmdurak <sinemin_email_adresi@gmail.com>
2026-09-15 17:45:16 +0300

    Update Google Drive Web App URL to new deployment AKfycbwbESzfJOMi1OCondiBM4QxZsHDiDUKfL8Iqtp6wIhAZof0G3jbJD7z497NUSd0tF8m/exec

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.post('/api/send-voice-note', upload.single('audio'), async (req, res) => {
     }
 
     // Google Drive Web App Forwarding
-    const googleDriveUrl = process.env.GOOGLE_DRIVE_WEB_APP_URL || 'https://script.google.com/macros/s/AKfycbzJFxA7kUq5Dx3DyM0HB6GU3QT_0FTKsHKjQYIuFIWkzi98wPNThEFzJeHo_xf4iOXx/exec';
+    const googleDriveUrl = process.env.GOOGLE_DRIVE_WEB_APP_URL || 'https://script.google.com/macros/s/AKfycbwbESzfJOMi1OCondiBM4QxZsHDiDUKfL8Iqtp6wIhAZof0G3jbJD7z497NUSd0tF8m/exec';
     let driveUploaded = false;
 
     if (googleDriveUrl) {
